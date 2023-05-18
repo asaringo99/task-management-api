09a21e098762dcc3cf6f801c40ea0ce1f8f196f0
asaringo99 <ka99code@gmail.com>
2023-05-19 01:05:19 +0900

    conditionの追加

---
+++ backend/internal/application/usecase/task/delete/condition/condition.go
@@ -0,0 +1,15 @@
+package condition
+
+import domain "github.com/asaringo99/task_management/internal/domain/entity"
+
+type TaskDeleteCondition struct {
+	Userid domain.Userid
+	Taskid domain.Taskid
+}
+
+func NewTaskDeleteCondition(userid int, taskid int) TaskDeleteCondition {
+	return TaskDeleteCondition{
+		Userid: domain.NewUserid(userid),
+		Taskid: domain.NewTaskid(taskid),
+	}
+}
