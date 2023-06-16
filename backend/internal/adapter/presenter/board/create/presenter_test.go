2859035e8ff843bb5916cad947c7710f98dd0847
asaringo99 <ka99code@gmail.com>
2023-06-16 14:11:47 +0900

    [add] board presenter

---
+++ backend/internal/adapter/presenter/board/create/presenter_test.go
@@ -0,0 +1,41 @@
+package presenter
+
+import (
+	"reflect"
+	"testing"
+
+	usecase "github.com/asaringo99/task_management/internal/application/usecase/board/create"
+	domain "github.com/asaringo99/task_management/internal/domain/valueobject"
+)
+
+func TestBuild(t *testing.T) {
+	tests := []struct {
+		name   string
+		output usecase.BoardCreateUsecaseOutput
+		want   BoardCreatePresenterOutputDto
+	}{
+		{
+			name: "no diff1",
+			output: usecase.BoardCreateUsecaseOutput{
+				Boardid:  domain.NewId(1),
+				Userid:   domain.NewUserid(1),
+				Priority: domain.NewPriority(1),
+				Status:   domain.NewStatus("test"),
+			},
+			want: BoardCreatePresenterOutputDto{
+				Boardid:  1,
+				Userid:   1,
+				Priority: 1,
+				Status:   "test",
+			},
+		},
+		// TODO: Add Tests
+	}
+
+	for _, tc := range tests {
+		got := convert(tc.output)
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Fatalf("case: %s, expected: %v, got: %v", tc.name, tc.want, got)
+		}
+	}
+}
