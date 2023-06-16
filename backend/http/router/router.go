560f4ccb0ebe31a344c216b46560e2876f6c6e37
asaringo99 <ka99code@gmail.com>
2023-06-16 14:15:47 +0900

    [fix] router

--- backend/http/router/router.go
+++ backend/http/router/router.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/asaringo99/task_management/http/handler"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -9,6 +11,12 @@ import (
 func NewRouter(handler []handler.Handler) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowCredentials: true,
+	}))
 	for _, h := range handler {
 		h.AddEntryPoint(e)
 	}
