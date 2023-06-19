d3432f3347e171bd23532ce5c1e50a05fa1b6b27
asaringo99 <ka99code@gmail.com>
2023-06-19 21:10:25 +0900

    [fix] response body修正

--- backend/http/response/response.go
+++ backend/http/response/response.go
@@ -3,6 +3,7 @@ package response
 type ResponseBody struct {
 	Status string      `json:"status"`
 	Data   interface{} `json:"data,omitempty"`
+	Token  string      `json:"token"`
 	Error  string      `json:"error,omitempty"`
 }
 
