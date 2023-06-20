749ca4b0da907bb7facc11a8c9082192d7971979
asaringo99 <ka99code@gmail.com>
2023-06-20 09:28:01 +0900

    fix migrations, new column

--- backend/migrations/000003_board.up.sql
+++ backend/migrations/000003_board.up.sql
@@ -2,6 +2,7 @@
 CREATE TABLE IF NOT EXISTS `boards` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `userid` int(11) NOT NULL,
+    `tabid` int(11) NOT NULL,
     `status` VARCHAR(191) NOT NULL,
     `priority` int(11) NOT NULL,
     PRIMARY KEY (`id`)
