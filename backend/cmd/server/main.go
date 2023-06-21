082840a4ee5e39d036b761839c2cb8769483dd7f
asaringo99 <ka99code@gmail.com>
2023-06-22 00:19:47 +0900

    [update] main

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -15,14 +15,22 @@ import (
 	token_usecase "github.com/asaringo99/task_management/http/auth/usecase/token"
 	"github.com/asaringo99/task_management/http/handler"
 	board_handler "github.com/asaringo99/task_management/http/handler/board"
+	tab_handler "github.com/asaringo99/task_management/http/handler/tab"
 	task_handler "github.com/asaringo99/task_management/http/handler/task"
 	"github.com/asaringo99/task_management/http/router"
 	board_controller "github.com/asaringo99/task_management/internal/adapter/controller/board"
+	tab_controller "github.com/asaringo99/task_management/internal/adapter/controller/tab"
 	task_controller "github.com/asaringo99/task_management/internal/adapter/controller/task"
 	board_create_gateway "github.com/asaringo99/task_management/internal/adapter/gateway/board/create"
 	board_delete_gateway "github.com/asaringo99/task_management/internal/adapter/gateway/board/delete"
 	board_fetch_gateway "github.com/asaringo99/task_management/internal/adapter/gateway/board/fetch"
+	board_patch_gateway "github.com/asaringo99/task_management/internal/adapter/gateway/board/patch"
 	board_put_gateway "github.com/asaringo99/task_management/internal/adapter/gateway/board/put"
+	tab_create_gateway "github.com/asaringo99/task_management/internal/adapter/gateway/tab/create"
+	tab_delete_gateway "github.com/asaringo99/task_management/internal/adapter/gateway/tab/delete"
+	tab_fetch_gateway "github.com/asaringo99/task_management/internal/adapter/gateway/tab/fetch"
+	tab_patch_gateway "github.com/asaringo99/task_management/internal/adapter/gateway/tab/patch"
+	tab_put_gateway "github.com/asaringo99/task_management/internal/adapter/gateway/tab/put"
 	task_create_gateway "github.com/asaringo99/task_management/internal/adapter/gateway/task/create"
 	task_delete_gateway "github.com/asaringo99/task_management/internal/adapter/gateway/task/delete"
 	task_fetch_gateway "github.com/asaringo99/task_management/internal/adapter/gateway/task/fetch"
@@ -31,7 +39,13 @@ import (
 	board_create_repository "github.com/asaringo99/task_management/internal/application/repository/board/create"
 	board_delete_repository "github.com/asaringo99/task_management/internal/application/repository/board/delete"
 	board_fetch_repository "github.com/asaringo99/task_management/internal/application/repository/board/fetch"
+	board_patch_repository "github.com/asaringo99/task_management/internal/application/repository/board/patch"
 	board_put_repository "github.com/asaringo99/task_management/internal/application/repository/board/put"
+	tab_create_repository "github.com/asaringo99/task_management/internal/application/repository/tab/create"
+	tab_delete_repository "github.com/asaringo99/task_management/internal/application/repository/tab/delete"
+	tab_fetch_repository "github.com/asaringo99/task_management/internal/application/repository/tab/fetch"
+	tab_patch_repository "github.com/asaringo99/task_management/internal/application/repository/tab/patch"
+	tab_put_repository "github.com/asaringo99/task_management/internal/application/repository/tab/put"
 	task_create_repository "github.com/asaringo99/task_management/internal/application/repository/task/create"
 	task_delete_repository "github.com/asaringo99/task_management/internal/application/repository/task/delete"
 	task_fetch_repository "github.com/asaringo99/task_management/internal/application/repository/task/fetch"
@@ -40,7 +54,13 @@ import (
 	board_create_usecase "github.com/asaringo99/task_management/internal/application/usecase/board/create"
 	board_delete_usecase "github.com/asaringo99/task_management/internal/application/usecase/board/delete"
 	board_fetch_usecase "github.com/asaringo99/task_management/internal/application/usecase/board/fetch"
+	board_patch_usecase "github.com/asaringo99/task_management/internal/application/usecase/board/patch"
 	board_put_usecase "github.com/asaringo99/task_management/internal/application/usecase/board/put"
+	tab_create_usecase "github.com/asaringo99/task_management/internal/application/usecase/tab/create"
+	tab_delete_usecase "github.com/asaringo99/task_management/internal/application/usecase/tab/delete"
+	tab_fetch_usecase "github.com/asaringo99/task_management/internal/application/usecase/tab/fetch"
+	tab_patch_usecase "github.com/asaringo99/task_management/internal/application/usecase/tab/patch"
+	tab_put_usecase "github.com/asaringo99/task_management/internal/application/usecase/tab/put"
 	task_create_usecase "github.com/asaringo99/task_management/internal/application/usecase/task/create"
 	task_delete_usecase "github.com/asaringo99/task_management/internal/application/usecase/task/delete"
 	task_fetch_usecase "github.com/asaringo99/task_management/internal/application/usecase/task/fetch"
@@ -110,7 +130,39 @@ func main() {
 			board_put_repository.NewBoardPutRepository,
 			board_put_gateway.NewBoardPutGateway,
 
+			board_patch_usecase.NewBoardPatchUsecase,
+			board_patch_usecase.NewBoardPatchInteractor,
+			board_patch_repository.NewBoardPatchRepository,
+			board_patch_gateway.NewBoardPatchGateway,
+
 			board_controller.NewBoardController,
+
+			tab_create_usecase.NewTabCreateUsecase,
+			tab_create_usecase.NewTabCreateInteractor,
+			tab_create_repository.NewTabCreateRepository,
+			tab_create_gateway.NewTabCreateGateway,
+
+			tab_fetch_usecase.NewFetchGetUsecase,
+			tab_fetch_usecase.NewTabFetchInteractor,
+			tab_fetch_repository.NewTabFetchRepository,
+			tab_fetch_gateway.NewTabFetchGateway,
+
+			tab_delete_usecase.NewTabDeleteUsecase,
+			tab_delete_usecase.NewTabDeleteInteractor,
+			tab_delete_repository.NewTabDeleteRepository,
+			tab_delete_gateway.NewTabDeleteGateway,
+
+			tab_put_usecase.NewTabPutUsecase,
+			tab_put_usecase.NewTabPutInteractor,
+			tab_put_repository.NewTabPutRepository,
+			tab_put_gateway.NewTabPutGateway,
+
+			tab_patch_usecase.NewTabPatchUsecase,
+			tab_patch_usecase.NewTabPatchInteractor,
+			tab_patch_repository.NewTabPatchRepository,
+			tab_patch_gateway.NewTabPatchGateway,
+
+			tab_controller.NewTabController,
 
 			router.NewEchoServer,
 			fx.Annotate(
@@ -119,6 +171,7 @@ func main() {
 			),
 			handler.AsHandler(task_handler.NewTaskHandler),
 			handler.AsHandler(board_handler.NewBoardHandler),
+			handler.AsHandler(tab_handler.NewTabHandler),
 			handler.AsHandler(handler.NewSignUpHandler),
 			handler.AsHandler(handler.NewLoginHandler),
 		),
